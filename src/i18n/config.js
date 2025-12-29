75180d076cfa29a5284aa371ee08841fc124011e
miaki3457 <buki2126@msn.com>
2025-12-29 12:07:52 +0800

    fix: 強制降級 i18next 至 v21 以相容本地開發環境

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -3,7 +3,10 @@ import i18next from 'i18next'
 i18next.init({
   // 優先從 localStorage 讀取語言，如果沒有則預設為 'en'
   lng: localStorage.getItem('i18nextLng') || 'en', 
-  debug: false
+  debug: false,
+  resources: {
+    
+  }
 })
 
 export default i18next
